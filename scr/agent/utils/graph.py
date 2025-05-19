49fabc0b97dd214c56ff4fd9499d487a17792228
Sebastian Schmülling <sebastian.schmuelling@gmail.com>
2025-05-19 09:12:12 +0200

    - Fixed LtM experiment issue with wrong token tracking for first three experiemts.

--- scr/agent/utils/graph.py
+++ scr/agent/utils/graph.py
@@ -33,28 +33,28 @@ def create_graph(config: Optional[Configuration] = None) -> Graph:
     # Add nodes with wrapper functions
     workflow.add_node("question_understanding", question_understanding)
     workflow.add_node("entity_resolution", retrieve_entities_classes)
-    #workflow.add_node("example_resolution", retrieve_examples)
-    #workflow.add_node("planning", plan_query)
-    #workflow.add_node("pattern", generate_and_validate_blocks)
-    #workflow.add_node("assembler", assemble_query)
-    workflow.add_node("sparql_query_construction", query_generator)
+    workflow.add_node("example_resolution", retrieve_examples)
+    workflow.add_node("planning", plan_query)
+    workflow.add_node("pattern", generate_and_validate_blocks)
+    workflow.add_node("assembler", assemble_query)
+    #workflow.add_node("sparql_query_construction", query_generator)
     #workflow.add_node("sparql_query_construction", query_generator_few_shot_cot)
 
     # Define edges
     workflow.add_edge("question_understanding", "entity_resolution")
-    workflow.add_edge("entity_resolution", "sparql_query_construction")
-    #workflow.add_edge("entity_resolution", "example_resolution")
-    #workflow.add_edge("example_resolution", "planning")
-    #workflow.add_edge("planning", "pattern")
-    #workflow.add_edge("pattern", "assembler")
+    #workflow.add_edge("entity_resolution", "sparql_query_construction")
+    workflow.add_edge("entity_resolution", "example_resolution")
+    workflow.add_edge("example_resolution", "planning")
+    workflow.add_edge("planning", "pattern")
+    workflow.add_edge("pattern", "assembler")
     #workflow.add_edge("entity_resolution", "sparql_query_construction")
 
     # Set entry point
     workflow.set_entry_point("question_understanding")
 
     # Set exit point
-    workflow.set_finish_point("sparql_query_construction")
-    #workflow.set_finish_point("assembler")
+    #workflow.set_finish_point("sparql_query_construction")
+    workflow.set_finish_point("assembler")
 
     # Compile the graph
     graph = workflow.compile()
