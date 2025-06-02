b2110d47bbcd63c6cbcec5749acecddad560b00a
Sebastian Schmülling <sebastian.schmuelling@gmail.com>
2025-06-02 18:39:56 +0200

    - Removed hard coded roots for experimental runs.

--- experiments/experiment_runner_functions/run_agent_evaluation.py
+++ experiments/experiment_runner_functions/run_agent_evaluation.py
@@ -4,7 +4,9 @@ import asyncio
 import os
 import traceback
 import sys
-sys.path.append('/Users/sebastian/Documents/Bachelor Thesis/sparql-rag-agent/sparql-rag-agent')
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, '..', '..'))
+sys.path.append(project_root)
 from experiments.utilities.evaluation import AgentEvaluator
 
 async def main():
@@ -12,11 +14,11 @@ async def main():
     Run the agent evaluation with the exact configuration from the Jupyter notebook.
     """
     endpoint_sets = { "SwissLipids": [], "Uniprot": [], "Rhea": [] }
-    experiment_dir = "experiments/experiment_data"
+    experiment_dir = os.path.join(project_root, "experiments/experiment_data")
     project_name = "pattern_assembler_v1.0_gemini-2.0-flash"
     timeout = 300
     tracked_token_nodes = ["question_understanding", "planning", "pattern", "assembler"]
-    dataset_dir = "/Users/sebastian/Documents/Bachelor Thesis/sparql-rag-agent/sparql-rag-agent/experiments/federated_sparql_dataset/examples_federated_19.04.2025"
+    dataset_dir = os.path.join(project_root, "experiments/federated_sparql_dataset/examples_federated_19.04.2025")
     
     evaluator = AgentEvaluator(
         endpoint_sets=endpoint_sets,
